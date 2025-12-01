30f4253f987c44e138b8405199c4fa25b0a53f7d
Anastasia Shiganova <Tyuiu.ShiganovaAV@outlook.com>
2025-12-02 01:15:34 +0500

    .

--- Tyuiu.ShiganovaAV.Sprint6.Task3.V11/FormMain.Designer.cs
+++ Tyuiu.ShiganovaAV.Sprint6.Task3.V11/FormMain.Designer.cs
@@ -9,117 +9,51 @@ namespace Tyuiu.ShiganovaAV.Sprint6.Task3.V11
     {
         private DataService ds = new DataService();
 
-        private int[,] matrix = new int[5, 5]
-        {
-            { 27, -15, 14,  2, 27 },
-            { -8,  14, -10, 33,  0 },
-            {  1,   7, -11, -11, 23 },
-            { -13, -20, 15, -16, 34 },
-            { -3,   1, -1,   5,  1 }
-        };
-
         public FormMain()
         {
             InitializeComponent();
-
             ShowCondition();
-
-            ShowMatrix();
         }
 
         private void ShowCondition()
         {
-            textBoxCondition_SAV.Text = "Дан массив 5 на 5 элементов.\r\n" +
-                                       "Выполнить сортировку по возрастанию в первом столбце.\r\n\r\n" +
-                                       "Исходный массив:\r\n" +
-                                       "27 -15  14   2  27\r\n" +
-                                       "-8  14 -10  33   0\r\n" +
-                                       "1   7 -11 -11  23\r\n" +
-                                       "-13 -20  15 -16  34\r\n" +
-                                       "-3   1 -1   5   1";
+            textBoxCondition_SAV.Text = "Дан массив 5 на 5 элементов.\r\nВыполнить сортировку по возрастанию в первом столбце.\r\n\r\nИсходный массив:\r\n27 -15  14   2  27\r\n-8  14 -10  33   0\r\n1   7 -11 -11  23\r\n-13 -20  15 -16  34\r\n-3   1 -1   5   1";
         }
 
-        private void ShowMatrix()
-        {
-            dataGridViewMatrix_SAV.Rows.Clear();
-            dataGridViewMatrix_SAV.Columns.Clear();
-
-            for (int j = 0; j < 5; j++)
-            {
-                dataGridViewMatrix_SAV.Columns.Add($"col{j}", $"");
-                dataGridViewMatrix_SAV.Columns[j].Width = 40;
-                dataGridViewMatrix_SAV.Columns[j].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            }
-
-            for (int i = 0; i < 5; i++)
-            {
-                dataGridViewMatrix_SAV.Rows.Add();
-                for (int j = 0; j < 5; j++)
-                {
-                    dataGridViewMatrix_SAV.Rows[i].Cells[j].Value = matrix[i, j];
-                }
-            }
-        }
-
-        private void buttonExecute_SAV_Click(object sender, EventArgs e)
+        private void buttonDone_SAV_Click(object sender, EventArgs e)
         {
             try
             {
+                int[,] matrix = ds.GetMatrix();
                 int[,] sortedMatrix = ds.Calculate(matrix);
-
-                ShowResultMatrix(sortedMatrix);
-
-                ShowResultText(sortedMatrix);
+                ShowResult(sortedMatrix);
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка",
-                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
-        private void ShowResultMatrix(int[,] sortedMatrix)
-        {
-            dataGridViewResult_SAV.Rows.Clear();
-            dataGridViewResult_SAV.Columns.Clear();
-
-            for (int j = 0; j < 5; j++)
-            {
-                dataGridViewResult_SAV.Columns.Add($"col{j}", $"");
-                dataGridViewResult_SAV.Columns[j].Width = 40;
-                dataGridViewResult_SAV.Columns[j].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            }
-
-            for (int i = 0; i < 5; i++)
-            {
-                dataGridViewResult_SAV.Rows.Add();
-                for (int j = 0; j < 5; j++)
-                {
-                    dataGridViewResult_SAV.Rows[i].Cells[j].Value = sortedMatrix[i, j];
-                }
-            }
-        }
-
-        private void ShowResultText(int[,] sortedMatrix)
+        private void ShowResult(int[,] sortedMatrix)
         {
             textBoxResult_SAV.Clear();
-            textBoxResult_SAV.AppendText("Результат:" + Environment.NewLine);
-            textBoxResult_SAV.AppendText("Массив отсортирован по первому столбцу." + Environment.NewLine + Environment.NewLine);
+            textBoxResult_SAV.AppendText("Результат:" + Environment.NewLine + Environment.NewLine);
 
-            textBoxResult_SAV.AppendText("Первый столбец после сортировки:" + Environment.NewLine);
             for (int i = 0; i < 5; i++)
             {
-                textBoxResult_SAV.AppendText(sortedMatrix[i, 0].ToString().PadLeft(4) + Environment.NewLine);
+                string row = "";
+                for (int j = 0; j < 5; j++)
+                {
+                    row += sortedMatrix[i, j].ToString().PadLeft(4);
+                }
+                textBoxResult_SAV.AppendText(row + Environment.NewLine);
             }
-        }
 
-        private void buttonHelp_SAV_Click(object sender, EventArgs e)
-        {
-            string info = "Таск 3 вариант 11 выполнил студент группы ПИНб-25-1\r\n" +
-                         "Шиганова Анастасия Владимировна";
-
-            MessageBox.Show(info, "Справка",
-                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            textBoxResult_SAV.AppendText(Environment.NewLine + "Первый столбец отсортирован:" + Environment.NewLine);
+            for (int i = 0; i < 5; i++)
+            {
+                textBoxResult_SAV.AppendText(sortedMatrix[i, 0].ToString().PadLeft(4) + " ");
+            }
         }
 
         private void textBoxStart_SAV_KeyPress(object sender, KeyPressEventArgs e)
@@ -128,6 +62,12 @@ namespace Tyuiu.ShiganovaAV.Sprint6.Task3.V11
             {
                 e.Handled = true;
             }
+        }
+
+        private void buttonHelp_SAV_Click(object sender, EventArgs e)
+        {
+            string info = "Таск 3 вариант 11 выполнил студент группы ПИНб-25-1\r\nШиганова Анастасия Владимировна";
+            MessageBox.Show(info, "Справка", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
