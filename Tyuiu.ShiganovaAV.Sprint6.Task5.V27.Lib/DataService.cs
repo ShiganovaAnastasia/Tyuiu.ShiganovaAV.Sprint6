1b80f64ed6a6d5690eaa798259ecce3adecbc361
Anastasia Shiganova <Tyuiu.ShiganovaAV@outlook.com>
2025-12-02 16:21:54 +0500

    .

--- Tyuiu.ShiganovaAV.Sprint6.Task5.V27.Lib/DataService.cs
+++ Tyuiu.ShiganovaAV.Sprint6.Task5.V27.Lib/DataService.cs
@@ -2,10 +2,10 @@
 using System.Globalization;
 using System.IO;
 using System.Collections.Generic;
-
+using tyuiu.cources.programming.interfaces.Sprint6;
 namespace Tyuiu.ShiganovaAV.Sprint6.Task5.V27.Lib
 {
-    public class DataService
+    public class DataService:ISprint6Task5V27
     {
         public double[] LoadFromDataFile(string path)
         {
